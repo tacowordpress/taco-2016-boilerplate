772b0430151ddf93a18e828f5b057aef6f24b4e4
Jasand <jasand@vermilion.com>
2015-12-17 22:33:02 -0700

    removed functions taco

--- html/wp-content/themes/vermilion-app/functions.php
+++ html/wp-content/themes/vermilion-app/functions.php
@@ -1,17 +1,16 @@
 <?php
 
-// autoload vendor files
-require_once realpath(__DIR__.'/taco-app/vendor/autoload.php');
-
-// Initialize Taco
-\Taco\Loader::init();
-
-require_once dirname(__FILE__).'/taco-app/config.php';
+// load core app files through taco-app.php
 require_once dirname(__FILE__).'/taco-app/taco-app.php';
 
-// load any frontned files for composer packages
-\TacoApp\CustomLoader::init();
+// assign aliases for common util classes
+class Arr extends \AppLibrary\Arr {}
+class Collection extends \AppLibrary\Collection {}
+class Color extends \AppLibrary\Color {}
+class Html extends \AppLibrary\Html {}
+class Num extends \AppLibrary\Num {}
+class Str extends \AppLibrary\Str {}
+class States extends \AppLibrary\States {}
 
-require_once dirname(__FILE__).'/taco-app/functions-taco.php';
 require_once dirname(__FILE__).'/taco-app/functions-generic.php';
 require_once dirname(__FILE__).'/taco-app/functions-app.php';
