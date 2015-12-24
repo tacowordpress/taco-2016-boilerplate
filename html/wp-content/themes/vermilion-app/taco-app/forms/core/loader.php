7c0abca21321cb881c2c0090493b145677913711
Jasand <jasand@vermilion.com>
2015-12-23 17:59:31 -0700

    load all form core files

---
+++ html/wp-content/themes/vermilion-app/taco-app/forms/core/loader.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__.'/FormTemplate.php';
+require_once __DIR__.'/FieldsPool.php';
+require_once __DIR__.'/FormConfBase.php';
+require_once __DIR__.'/FormEntry.php';
+require_once __DIR__.'/FormField.php';
