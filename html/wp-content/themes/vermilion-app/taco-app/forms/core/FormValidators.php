ff14446f0334344477fcb9cce4e95af30f8c44c2
Jasand <jasand@vermilion.com>
2016-01-02 11:28:00 -0700

    add dev comments for form validators

--- html/wp-content/themes/vermilion-app/taco-app/forms/core/FormValidators.php
+++ html/wp-content/themes/vermilion-app/taco-app/forms/core/FormValidators.php
@@ -1,6 +1,20 @@
 <?php
 
+/* All of these methods are for checking a value for a certain condition(s)
+ * Every method has the parameters "$value" and "property_value".
+ * The $property_value allows for things like
+ *   e.g. comparing string length against a maxlength (you name it)
+ */
+
+
 trait FormValidators {
+
+
+  /**
+   * check if a field is required
+   * @param  $value string
+   * @return $property_value string
+   */
   public static function checkRequired($value, $property_value=null) {
     if(empty($value)) {
       return true;
@@ -8,6 +22,12 @@ trait FormValidators {
     return false;
   }
 
+
+  /**
+   * check if a field value is a valid email
+   * @param  $value string
+   * @return $property_value string
+   */
   public static function checkEmail($value, $property_value=null) {
     if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
       return true;
@@ -15,6 +35,12 @@ trait FormValidators {
     return false;
   }
 
+
+  /**
+   * check if a field is a valid url
+   * @param  $value string
+   * @return $property_value string
+   */
   public static function checkURL($value, $property_value=null) {
     if(!strlen($value)) return true;
     if(!filter_var($value, FILTER_VALIDATE_URL)) {
@@ -23,11 +49,23 @@ trait FormValidators {
     return true;
   }
 
+
+  /**
+   * check if the value doesn't exceed a certain maxlength
+   * @param  $value string
+   * @return $property_value string
+   */
   public static function checkMaxLength($value, $property_value=null) {
     if(strlen($value) > $property_value) return true;
     return false;
   }
 
+
+  /**
+   * check if a field is a valid U.S. zip
+   * @param  $value string
+   * @return $property_value string
+   */
   public static function checkZip($value, $property_value=null) {
     $is_zip_regex = '^\d{5}([\-]?\d{4})?$';
     if(!preg_match("/".$is_zip_regex."/i", $value)) {
