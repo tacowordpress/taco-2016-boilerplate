6fedbfad0923104c54f46f817718335c59221bbe
Jasand <jasand@vermilion.com>
2016-01-03 16:48:40 -0700

    path fix, remove test file

--- html/wp-content/themes/taco-theme/app/forms/forms-defaults.php
+++ html/wp-content/themes/taco-theme/app/forms/forms-defaults.php
@@ -4,7 +4,7 @@
  *   in the form config (hardcoded), or if the admin adds values in the wp-admin (if applicable).
  */
 return array(
-  'form_action' => '/wp-content/themes/vermilion-app/taco-app/forms/core/FormSubmit.php',
+  'form_action' => '/wp-content/themes/taco-theme/app/forms/core/FormSubmit.php',
   'error_message' => 'There were some errors with your form submission. Please correct and try again.',
   'success_message' => 'Thanks for your message',
 );
