feaba1a35780cbdf6c749a84ec2b3d9f400d4ebc
Jasand <jasand@vermilion.com>
2016-01-03 23:35:34 -0700

    form template doc block fix and removed template param

--- html/wp-content/themes/taco-theme/app/forms/core/FormTemplate.php
+++ html/wp-content/themes/taco-theme/app/forms/core/FormTemplate.php
@@ -5,25 +5,21 @@ Class FormTemplate extends \AppLibrary\QuickTemplate {
   /**
    * Uses output buffer in callback function to return a string of new values
    * @param array $data
-   * @param function $callback
-   * @param bool $return_content (returned by reference)
-   * @param object $object (pass the form object into the custom template)
+   * @param $callback callable
+   * @param $return_content bool|output buffer (returned by reference)
+   * @param $object object (pass the form object into the custom template)
    * @return string
    */
-  public static function create($data, $callback, $template=null, &$return_content=false, $object=null) {
+  public static function create($data, $callback, &$return_content=false, $object=null) {
     ob_start(function($buffer) use ($data, &$return_content) {
       $self = __CLASS__;
       $updated = $self::template_iterator($data, $buffer);
-      
       if(is_null($return_content)) {
         $return_content = $updated;
       } else {
         return $updated;
       }
     });
-    if($template) {
-      echo $template;
-    }
     $callback($object);
     ob_end_flush();
   }
