31adbc2b39527b8dbd06a50291bb57ef5aabc12d
Alan Dague-Greene <alan@basicrecipe.com>
2016-02-04 13:28:38 -0700

    Simplify getting theme version

--- html/wp-content/themes/taco-theme/app/config.php
+++ html/wp-content/themes/taco-theme/app/config.php
@@ -2,6 +2,6 @@
 
 // Cache busting
 // This automatically pulls from main.scss
-define('THEME_VERSION', (ENVIRONMENT !== ENVIRONMENT_PROD) ? mt_rand() : (int) preg_replace('/((?:.*)\$version\:[\s]{1,}([\d]{1,});(?:.*))/si', '$2', file_get_contents(dirname(__FILE__).'/_/scss/main.scss')));
+define('THEME_VERSION', (ENVIRONMENT !== ENVIRONMENT_PROD) ? mt_rand() : (int) preg_replace('/((?:.*)\$version\:\s+(\d+);(?:.*))/si', '$2', file_get_contents(__DIR__.'/_/scss/main.scss')));
 define('THEME_SUFFIX', sprintf('?v=%s', THEME_VERSION));
 define('USER_SUPER_ADMIN', 'vermilion_admin'); // vermilion_admin
