366a7a45111694b569330ee2a643938bb95d5e18
Michael <michael@vermilion.com>
2016-02-24 15:40:22 -0700

    Remove html comments - Remove or replace html comments with php comments #19

--- html/wp-content/themes/taco-theme/includes/incl-head.php
+++ html/wp-content/themes/taco-theme/includes/incl-head.php
@@ -6,21 +6,16 @@ $theme = AppOption::getInstance();
 <head>
   <title><?php echo app_get_page_title(); ?></title>
 
-  <!-- meta -->
   <?php include __DIR__.'/incl-open-graph-meta.php'; ?>
-
 
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <!-- fonts -->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
-  <!-- icons -->
   <?php echo get_app_icons(); ?>
 
-  <!-- scripts -->
   <script src="<?php echo get_asset_path('lib/modernizr/modernizr.js'); ?>"></script>
 
   <?php wp_head(); ?>
