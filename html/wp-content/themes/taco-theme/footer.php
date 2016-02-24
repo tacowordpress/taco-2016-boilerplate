f7daed0a3c8ebc9aa01323ab1aa6acc17076be0b
Brittany <britt.ansay@gmail.com>
2016-02-24 15:45:05 -0700

    remove legacy FlashData stuff from copeland

--- html/wp-content/themes/taco-theme/footer.php
+++ html/wp-content/themes/taco-theme/footer.php
@@ -6,13 +6,5 @@ something needed in the footer?
 
 <?php wp_footer(); ?>
 
-<script>
-  //$(document).foundation();
-</script>
 </body>
 </html>
-<?php
-if(class_exists('FlashData')) {
-  $flash_data = new FlashData;
-  $flash_data->flush();
-}
