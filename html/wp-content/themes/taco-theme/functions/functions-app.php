210a4d4b945a53402492d700f703279cba53dd6d
Alan Dague-Greene <alan@basicrecipe.com>
2016-03-07 15:11:51 -0700

    Add filters for adding slugs to body and menu items

--- html/wp-content/themes/taco-theme/functions/functions-app.php
+++ html/wp-content/themes/taco-theme/functions/functions-app.php
@@ -274,3 +274,43 @@ add_filter('single_template', function() {
   }
   return __DIR__.sprintf('/../singles/single-%s.php', $post->post_type);
 });
+
+
+function add_slug_to_body_class($classes=[]) {
+  global $post;
+  $file_name = basename($_SERVER['SCRIPT_FILENAME'], '.php');
+  $queried_object = get_queried_object();
+  $is_term = (is_object($queried_object) && property_exists($queried_object, 'term_taxonomy_id'));
+  if(!$is_term && !is_null($post)) {
+    $classes[] = $post->post_name;
+  } else {
+    $classes[] = Str::machine($file_name, '-');
+  }
+  return $classes;
+}
+add_filter('body_class', 'add_slug_to_body_class');
+
+
+function add_slug_to_menu_item_class($menu_html) {
+  // Get menu item IDs and link slugs
+  preg_match_all('/menu-item-(\d+).*href="(?:(?:.*?)\/\/(?:.*?))?\/(.*?)\/?"/', $menu_html, $matches);
+  
+  // Combine match groups into array
+  $menu_items = array_combine($matches[1], $matches[2]);
+  
+  // Strip slugs down to last segment
+  $menu_items = array_map(function($el){
+    $slash_index = strrpos($el, '/');
+    return ($slash_index)
+      ? substr($el, $slash_index + 1)
+      : $el;
+  }, $menu_items);
+  
+  // Search/replace
+  foreach($menu_items as $menu_item_id => $link_slug) {
+    $menu_html = preg_replace('/menu-item-'.$menu_item_id.'">/', 'menu-item-'.$menu_item_id.' menu-item-'.$link_slug.'">', $menu_html, 1);
+  }
+  return $menu_html;
+}
+add_filter('wp_nav_menu', 'add_slug_to_menu_item_class');
+
