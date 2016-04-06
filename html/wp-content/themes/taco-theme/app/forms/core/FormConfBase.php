b7f676ea8d2f306a01599026d3e161f44c1e9c28
Jasand Pereza <jasandpereza@gmail.com>
2016-04-06 09:25:39 -0600

    remove fields in development

--- html/wp-content/themes/taco-theme/app/forms/core/FormConfBase.php
+++ html/wp-content/themes/taco-theme/app/forms/core/FormConfBase.php
@@ -4,23 +4,13 @@ class FormConfBase extends \Taco\Post {
 
   public function getFields() {
     return array(
-      'unique_id' => ['type' => 'text'],
+      'unique_id' => ['type' => 'hidden'],
       'fields' => array('type' => 'hidden'),
       'form_description' => array('type' => 'textarea'),
-      'do_not_track_anything' => array(
-        'type' => 'checkbox'
-      ),
-      'auto_remove_database_records_after' => array(
-        'type' => 'text',
-        'placeholder' => 'days'
-      ),
       'admin_emails' => array('type' => 'text'),
       'success_redirect_url' => array(
         'type' => 'url',
         'description' => 'If this field is filled out, the user will not see the sucess message and instead will be directed to the url specified'
-      ),
-      'send_daily_report_of_entries_to_admin' => array(
-        'type' => 'checkbox'
       ),
       'form_success_message' => array(
         'type' => 'textarea',
