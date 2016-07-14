6cc5cd04dddfbc21cb039d8400dc81e573520081
Jasand <jasandpereza@gmail.com>
2016-07-14 17:22:49 -0600

    extract theme

--- boilerplate/wp-content/themes/taco-theme/404.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-404 not found
-
-<?php get_footer(); ?>
--- boilerplate/wp-content/themes/taco-theme/app/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Cache busting
-// This automatically pulls from main.scss
-define('THEME_VERSION', (ENVIRONMENT !== ENVIRONMENT_PROD) ? mt_rand() : (int) preg_replace('/((?:.*)\$version\:\s+(\d+);(?:.*))/si', '$2', file_get_contents(__DIR__.'/_/scss/main.scss')));
-define('THEME_SUFFIX', sprintf('?v=%s', THEME_VERSION));
-define('USER_SUPER_ADMIN', 'vermilion_admin'); // vermilion_admin
