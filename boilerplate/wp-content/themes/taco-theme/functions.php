b67bd6fbdb3f2974e57fa28ce6fe2527dc1eaa97
Alan Dague-Greene <alan@basicrecipe.com>
2016-07-15 13:45:19 -0600

    Update namespace for Util classes

--- boilerplate/wp-content/themes/taco-theme/functions.php
+++ boilerplate/wp-content/themes/taco-theme/functions.php
@@ -6,13 +6,13 @@ require_once __DIR__.'/app/config.php';
 require_once __DIR__.'/app/taco-app.php';
 
 // assign aliases for common util classes
-class Arr extends \AppLibrary\Arr {}
-class Collection extends \AppLibrary\Collection {}
-class Color extends \AppLibrary\Color {}
-class Html extends \AppLibrary\Html {}
-class Num extends \AppLibrary\Num {}
-class Obj extends \AppLibrary\Obj {}
-class Str extends \AppLibrary\Str {}
+class Arr extends \Taco\Util\Arr {}
+class Collection extends \Taco\Util\Collection {}
+class Color extends \Taco\Util\Color {}
+class Html extends \Taco\Util\Html {}
+class Num extends \Taco\Util\Num {}
+class Obj extends \Taco\Util\Obj {}
+class Str extends \Taco\Util\Str {}
 class States extends \AppLibrary\States {}
 
 require_once __DIR__.'/functions/functions-generic.php';
